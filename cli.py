d68fe7d545d17649eb45380cee86b6da08a43282
Rasmus Einarsson <mr@rasmuseinarsson.se>
2016-09-20 20:01:35 +0200

    Build new CLI

--- cli.py
+++ cli.py
@@ -1,137 +1,185 @@
-import click
 import time
 import csv
+import click
+import prompt_toolkit
+from prompt_toolkit.shortcuts import prompt
+from prompt_toolkit.shortcuts import clear
+from prompt_toolkit.shortcuts import create_prompt_application
+from prompt_toolkit.shortcuts import create_eventloop
+from prompt_toolkit.shortcuts import create_prompt_layout
+from prompt_toolkit.interface import CommandLineInterface
+from prompt_toolkit.token import Token
+from prompt_toolkit.validation import Validator, ValidationError
+from prompt_toolkit.filters import Condition, Filter
+from prompt_toolkit.key_binding.manager import KeyBindingManager
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.styles import style_from_dict
 
-PROPERTY_NAMES = ["Salt","Knaprig","Rostad","Flottig"]
-NUM_PEANUTS = 20
-TBF_CHOICES = ['APK', 'KFB'] + ['A{:2}'.format(i) for i in range(30)]
+style = style_from_dict({
+    Token.Toolbar: '#333333 bg:#cccccc',
+})
+
+prompt_toolkit.key_binding.manager.KeyBindingManager(
+    registry=None, enable_vi_mode=None, get_search_state=None,
+    enable_abort_and_exit_bindings=False, enable_system_bindings=False,
+    enable_search=False, enable_open_in_editor=False,
+    enable_extra_page_navigation=False, enable_auto_suggest_bindings=False,
+    enable_all=True)
+
+TBF_VALUES = ['a', 'c']
+N_PEANUTS = 20
+PROPERTY_NAMES = ['Salt', 'Knaprig', 'Rostad', 'Flottig']
 
 COLUMNS = (
     ["Timestamp","Din TBF","Vilken jordnöt?"]
     + PROPERTY_NAMES
     + ["Betygsätt totalupplevelsen","Kommentar"])
 
+CSV_SETTINGS = dict(delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 def create_file(path):
     with open(path, 'x') as f:
-        w = csv.writer(
-            f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        w = csv.writer(f, **CSV_SETTINGS)
         w.writerow(COLUMNS)
 
-def save(path, tbf, peanut, score, properties, comment):
-    row = (
-        [time.strftime('%Y-%m-%d %H:%M:%S %z'), tbf, peanut]
-        + [properties[key] for key in PROPERTY_NAMES]
-        + [score, comment])
+def save(path, item):
+    item['Timestamp'] = time.strftime('%Y-%m-%d %H:%M:%S %z')
+    row = [item[k] for k in COLUMNS]
     with open(path, 'a') as f:
-        w = csv.writer(
-            f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        w = csv.writer(f, **CSV_SETTINGS)
         w.writerow(row)
-        return True
 
-def get_TBF():
-    value = None
-    error = None
-    while True:
-        if error:
-            click.echo(error)
-            error = None
-        value = click.prompt('TBF', type=str)
-        value = value.upper()
-        if value in TBF_CHOICES:
-            break
+class Restart(Exception): pass
+
+class NumberValidator(Validator):
+    def __init__(self, min_, max_):
+        self._min = min_
+        self._max = max_
+
+    def validate(self, document):
+        text = document.text
+
+        i = 0
+        if text.isdigit():
+            if self._min <= int(text) <= self._max:
+                return
         else:
-            error = '"{}" är inte en godkänd TBF'.format(value)
-    return value
+            for i, c in enumerate(text):
+                if not c.isdigit():
+                    break
 
+        raise ValidationError(
+            message='Mata in ett tal ({}-{})'.format(self._min, self._max),
+            cursor_position=i)
 
-def ask_integer(q, min_=1, max_=7):
-    value = None
-    while True:
-        value = click.prompt('{} [{}-{}]'.format(q, min_, max_), type=int)
-        if min_ <= value <= max_:
-            return value
+class SetValidator(Validator):
+    def __init__(self, values):
+        self._values = set(values)
 
-def space_pad_right(s, l):
-    l0 = len(s)
-    return s + ' ' * (l - l0)
+    def validate(self, document):
+        text = document.text
 
-def ask_confirm():
-    value = None
-    while True:
-        value = click.prompt('Spara detta svar? [j/n] ', prompt_suffix='', type=str)
-        if value[0].lower() in ['j', 'y']:
-            return True
-        elif value[0].lower() in ['n']:
-            return False
-
-
-def collect_item():
-    click.echo('BETYGSSÄTT EN JORDNÖT')
-    click.echo('')
-
-    person_id = get_TBF()
-    peanut_id = ask_integer('Jordnöt', 1, NUM_PEANUTS)
-
-    click.echo('')
-    click.echo('Din åsikt')
-    click.echo('------------------')
-    score = ask_integer('Betygssätt totalupplevelsen', 1, 7)
-
-    click.echo('')
-    click.echo('Beskriv jordnöten')
-    click.echo('------------------')
-
-    properties = {}
-    for k in PROPERTY_NAMES:
-        properties[k] = ask_integer(k, 1, 7)
-
-    comment = click.prompt('Kommentar', type=str, default='')
-
-
-    def echo_item(label, value):
-        click.echo('{} {}'.format(space_pad_right(label + ':', 11), value))
-
-    click.echo('')
-    click.echo('Bekräfta uppgifter')
-    click.echo('------------------')
-    
-    echo_item('Din TBF', person_id)
-    echo_item('Jordnöt', peanut_id)
-    echo_item('Betyg', score)
-    for k in PROPERTY_NAMES:
-        echo_item(k, properties[k])
-    echo_item('Kommentar', comment)
-
-    click.echo('')
-    if ask_confirm():
-        return (person_id, 'J{}'.format(peanut_id), score, properties, comment)
-    else:
-        raise click.Abort()
-    
-
+        if text not in self._values:
+            raise ValidationError(
+                message='Otillåtet värde',
+                cursor_position=len(text))
 
 @click.command()
 @click.argument('path', type=click.Path(exists=False))
-def collect_data(path):
+def main(path):
     create_file(path)
+
+    progression = [
+        ('asking', 'TBF'),
+        ('asking', 'Vilken jordnöt'),
+        ('asking', 'Salt'),
+    ]
+    context = {'items': []}
+    def get_state():
+        return context['current']
+    def set_state(s):
+        context['current'] = s
+
+    mgr = KeyBindingManager(enable_abort_and_exit_bindings=True)
+
+    def prompt_tokens(cli):
+        state = get_state()
+        if state[0] == 'asking':
+            return [
+                (Token, state[1]),
+                (Token.Colon, ':'),
+                (Token.Space, ' ')
+                ]
+        if state[0] == 'canceling':
+            return [(Token, 'Avbryt? (j/n) ')]
+
+        raise RuntimeError(state)
+
+    def get_toolbar_tokens(cli):
+        return [(Token.Toolbar, 'Ctrl+C: Avbryt')]
+
+    @Condition
+    def is_canceling(cli):
+        return get_state()[0] == 'canceling'
+
+    @mgr.registry.add_binding('j', filter=is_canceling)
+    def _(event):
+        raise Restart()
+
+
+    @mgr.registry.add_binding('n', filter=is_canceling)
+    def _(event):
+        previous_state = get_state()[1]
+        set_state(previous_state)
+        event.cli.current_buffer.undo()
+
+
+    @mgr.registry.add_binding(Keys.ControlC)
+    def _(event):
+        event.cli.current_buffer.save_to_undo_stack()
+        event.cli.current_buffer.cursor_position = 0
+        event.cli.current_buffer.text = ''
+        set_state(('canceling', get_state()))
+
+
+    things = (
+        [
+        'NY JORDNÖT\n==============\n',
+        {'key': "Din TBF", 'v': SetValidator(TBF_VALUES)},
+        {'key': "Vilken jordnöt?", 'v': NumberValidator(1, N_PEANUTS)},
+        '\n\nDin åsikt\n-----------------\n',
+        {'key': "Betygsätt totalupplevelsen", 'v': NumberValidator(1, 7)},
+        '\n\nBeskriv jordnöten\n-----------------\n',
+        ]
+        +
+        [{'key': n, 'v': NumberValidator(1, 7)} for n in PROPERTY_NAMES]
+        +
+        [{'key': "Kommentar", 'v': None}]
+        )
+    
     while True:
         try:
-            item = collect_item()
-            if save(path, *item):
-                click.echo('Sparat!')
-            exit_status = ''
-        except click.Abort:
-            exit_status = 'Avbruten!'
-        
-        try:
-            click.echo(exit_status)
-            click.confirm('Tryck ENTER för att mata in ny jordnöt.', default=True, prompt_suffix='', show_default=False)
-        except click.Abort:
+            clear()
+            item = {}
+            for t in things:
+                if isinstance(t, str):
+                    print(t)
+                    continue
+
+                k, validator = t['key'], t['v']
+                set_state(('asking', k))
+                inp = prompt(
+                    get_prompt_tokens=prompt_tokens,
+                    get_bottom_toolbar_tokens=get_toolbar_tokens,
+                    key_bindings_registry=mgr.registry,
+                    style=style,
+                    validator=validator
+                    )
+                item[k] = inp
+            save(path, item)
+        except Restart:
             pass
-
-        click.clear()
-
-
+            
+    
 
 if __name__ == '__main__':
-    collect_data()
+    main()
