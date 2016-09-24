e63bf8c37b6c5826b43845d9c9a0ec0e70a7ceda
Rasmus Einarsson <mr@rasmuseinarsson.se>
2016-09-24 15:44:19 +0200

    Fix bug (type checking)

--- cli.py
+++ cli.py
@@ -207,7 +207,6 @@ def main(path):
                     validator=validator
                     )
                 item[k] = t['type_converter'](inp)
-                item[k] = inp
             set_state('confirm_done')
             prompt(
                     get_prompt_tokens=prompt_tokens,
